4aa42aea165c2d28f3b0a2ecab4762b463ac1087
Thomas Janke <janke@tu-berlin.de>
2023-05-09 17:28:38 +0200

    Add tilt-schema examples validator, move scripts to scripts/

--- validator-example.py
+++
@@ -1,9 +0,0 @@
-from jsonschema import validate
-import json
-
-with open('tilt-schema.json') as file:
-    schema = json.load(file)
-
-
-for example in schema["examples"]:
-  validate(instance=example, schema=schema)
